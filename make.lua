c110d9f1c862001080f037d1f607e1eece62a19c
broma0 <121064585+broma0@users.noreply.github.com>
2026-04-16 20:36:25 -0400

    update make.lua

--- make.lua
+++ make.lua
@@ -11,7 +11,7 @@ local env = {
   },
 }
 
-env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
+env.homepage = "https://github.com/birchpointswe/lua-" .. env.name
 env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
