83dd0390a46dc1fa35b5ca1996333d696f9c1a5f
broma0 <121064585+broma0@users.noreply.github.com>
2026-07-30 19:17:21 -0400

    bump version to 1.0.0-1

--- make.lua
+++ make.lua
@@ -1,12 +1,12 @@
 local env = {
   name = "santoku-socket",
-  version = "0.0.9-1",
+  version = "1.0.0-1",
   license = "MIT",
   public = true,
   dependencies = {
     "lua == 5.1",
-    "santoku >= 0.0.328-1",
-    "santoku-system >= 0.0.63-1",
+    "santoku >= 1.0.0, < 2.0.0",
+    "santoku-system >= 1.0.0, < 2.0.0",
     "luasocket >= 3.1.0-1",
     "luasec >= 1.3.2-1",
   },
